8e9dadc89dc593f803b054631039c6ee89853ad7
Phil Samuel <philipmayr@outlook.com>
2025-10-20 18:38:41 -0500

    Implement random prime number generator

---
+++ helper.rb
@@ -0,0 +1,16 @@
+# Factoring-Based Bit Commitment Scheme
+
+require "securerandom"
+require "prime"
+
+def draw_random_prime(bit_length = 16)
+    loop do
+        random_bits = SecureRandom.random_number(2 ** bit_length)
+
+        # Ensure the random number is both significantly large and odd
+        bit_mask = (1 << (bit_length - 1)) | 1
+        random_bits |= bit_mask
+
+        return random_bits if Prime.prime?(random_bits)
+    end
+end
