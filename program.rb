617896414ba01eecee70cff4b4cc228ab133fde1
Phil Samuel <philipmayr@outlook.com>
2025-10-20 18:34:36 -0500

    Change single quotes to double quotes in program.rb

--- program.rb
+++ program.rb
@@ -1,7 +1,7 @@
 # Factoring-Based Bit Commitment Scheme
 
-require 'securerandom'
-require 'prime'
+require "securerandom"
+require "prime"
 
 def draw_random_prime(bit_length = 16)
     loop do
@@ -10,7 +10,7 @@ def draw_random_prime(bit_length = 16)
         # Ensure the random number is both significantly large and odd
         bit_mask = (1 << (bit_length - 1)) | 1
         random_bits |= bit_mask
-    
+
         return random_bits if Prime.prime?(random_bits)
     end
 end
