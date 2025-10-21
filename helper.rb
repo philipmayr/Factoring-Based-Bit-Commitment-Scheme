179766f280cd96aa30a515b675a599f3a34d2bd5
Phil Samuel <philipmayr@outlook.com>
2025-10-20 19:27:33 -0500

    Refactor draw_random_prime method parameter spacing

--- helper.rb
+++ helper.rb
@@ -3,7 +3,7 @@
 require "securerandom"
 require "prime"
 
-def draw_random_prime(bit_length = 16)
+def draw_random_prime(bit_length=16)
     loop do
         random_bits = SecureRandom.random_number(2 ** bit_length)
 
